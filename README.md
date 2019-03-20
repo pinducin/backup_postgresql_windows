bc69955673dbf83e1dd91f046d5fd04f418f884b
PINDU <parruccial@yahoo.com.ar>
2019-03-20 06:56:04 -0300

    versión final

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Automatización del backup de PostgreSQL
 # Pasos
 
 - Modificar el script con la ruta correcta donde se encuentra el pg_dump.exe
+- Modificar el script con la ruta correcta donde se almacenará el archivo del backup
 - Modificar el pg_hba.conf para que al usuario postgres no le solicite contraseña desde la PC local.
 - Crear una tarea para que ejecute el script a una hora determinada todo los dias.
--- backup.bat
+++ backup.bat
@@ -1,4 +1,18 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::Script para realizar el backup de una base de datos del PostgreSQL
+::Deshabilito la salida por pantalla
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @echo off
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::Modificar las siguientes variables con las rutas correctas
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+set path="C:\Users\pindu\Desktop\backup_postgresql_windows"
+set db=postgres
+set ejecutable="C:\Program Files\PostgreSQL\11\bin\pg_dump.exe"
+set usuario=postgres
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::Obtención de los campos para armar el nombre del backup
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 set hour=%time:~0,2%
 if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
 set min=%time:~3,2%
@@ -6,10 +20,16 @@ if "%min:~0,1%" == " " set min=0%min:~1,1%
 set year=%date:~10,4%
 set month=%date:~4,2%
 set day=%date:~7,2%
-set filename=C:\backup\%year%%month%%day%-%hour%%min%-postgres.sql
-
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::Composición del nombre del archivo que va a contener el backup
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+set filename=%path%\%year%%month%%day%-%hour%%min%-%db%.sql
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::Inicio del backup
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 echo Iniciando el backup
-
-"C:\Program Files\PostgreSQL\11\bin\pg_dump.exe" -U postgres -f %filename% postgres
-
-echo Iniciando el backup
+%ejecutable% -U %usuario% -f %filename% %db%
+echo Backup finalizado
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::Finalización del backup
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
